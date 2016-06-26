65bf1514d0d79720afefb9736dc7f7ee651f22a2
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-06-26 19:54:37 -0400

    Added preliminary options processing. Refactored the monkey patch code. Refactored the help and header code. Factored out the gem description.

--- lib/vls.rb
+++ lib/vls.rb
@@ -1,40 +1,7 @@
 # coding: utf-8
 
+require_relative "vls/monkey_patch"
 require_relative "vls/version"
-
-#An addition to the Object class for vls support.
-class Object
-  #Get the vls version string from this module.
-  def to_vls_version_string
-    "version #{self.class} ???"
-  end
-end
-
-#An addition to the String class for vls support.
-class String
-  #Get this string as a vls version string.
-  def to_vls_version_string
-    self
-  end
-end
-
-#An addition to the Module class for vls support.
-class Module
-  #Get the vls version string from this module.
-  def to_vls_version_string
-    self::STRING
-  rescue
-    "version #{self} ???"
-  end
-end
-
-#An addition to the Array class for vls support.
-class Array
-  #Get the vls version string from this array.
-  def to_vls_version_string
-    (1..5) === self.length ? self.join('.') : super
-  end
-end
 
 # The Version LiSt utility module.
 module VersionLS
